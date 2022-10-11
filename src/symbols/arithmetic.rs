ef6a86de3f3f70b34e5e9c5ab6f72650bfb1eeed
François Michel <francois.michel@uclouvain.be>
2022-10-11 11:48:27 +0000

    fix gf256 pow

--- src/symbols/arithmetic.rs
+++ src/symbols/arithmetic.rs
@@ -27,7 +27,7 @@ pub fn add_mul(a: &mut [u8], coef: u8, b: &[u8]) {
 }
 
 pub fn pow(a: u8, mut exp: u8) -> u8 {
-    let mut res = 1;
+    let mut res = a;
     while exp > 1 {
         if exp & 1 == 0 {
             res = gf_tables::tables::GF256_MUL_TABLE[res as usize][res as usize];
