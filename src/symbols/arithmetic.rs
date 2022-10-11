5f17a4aaac46c1ad319534f1982f287f7d72d1c6
François Michel <francois.michel@uclouvain.be>
2022-10-11 14:04:05 +0000

    fix pow and add pow table

--- src/symbols/arithmetic.rs
+++ src/symbols/arithmetic.rs
@@ -25,27 +25,3 @@ pub fn add_mul(a: &mut [u8], coef: u8, b: &[u8]) {
         a[i] = a[i] ^ gf_tables::tables::GF256_MUL_TABLE[usize::from(coef)][usize::from(b[i])];
     }
 }
-
-pub fn pow(mut a: u8, mut exp: u8) -> u8 {
-    let mut res = 1;
-    while exp > 0 {
-        if exp & 1 == 0 {
-            a = gf_tables::tables::GF256_MUL_TABLE[a as usize][a as usize];
-            exp >>= 1;
-        } else {
-            res = gf_tables::tables::GF256_MUL_TABLE[res as usize][a as usize];
-            exp -= 1;
-        }
-    }
-    res
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::symbols::{arithmetic, gf_tables};
-
-    #[test]
-    fn test_pow() {
-        assert_eq!(arithmetic::pow(42, 4), gf_tables::mul(gf_tables::mul(42, 42), gf_tables::mul(42, 42)));
-    }
-}
